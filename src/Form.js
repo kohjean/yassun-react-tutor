958760ac5c062b118c8317104d9c7a1566a63ef3
ishii <reservist088@gmail.com>
2021-06-09 20:02:08 +0900

    フォームの利用

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-export class Form extends React.Component {
-  render() {
-    return <div>フォームです</div>
+export const Form = () => {
+  const [text, setText] = useState('');
+  const submitForm = (event) => {
+    event.preventDefault();
+    console.log(text);
   }
+
+  return (
+    <div>
+      <h4>新しい言語の追加</h4>
+      <form onSubmit={submitForm}>
+        <div>
+          <input 
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)} 
+          />
+        </div>
+        <div>
+          <button>追加</button>
+        </div>
+      </form>
+    </div>
+  )
 }
